3bf2cc0f3cc290fd54c9c9ce5d2a0be6dccab8e7
Chaitanya Sohani <chaitanyasohani@Chaitanyas-MacBook-Air.local>
2025-05-12 00:26:43 -0600

    Contact page rough done

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import '../styles/Contact.css'
 
 const Contact = () => {
   return (
-    <div>
-      Contact
-    </div>
+      <div className="contact-section">
+        <div className="contact-left">
+          <h1>Have any questions for us?</h1>
+          <h4>Send us a message!</h4>
+        </div>
+        <div className="contact-right">
+          <form className="contact-form">
+            <input type="text" placeholder="Name" />
+            <input type="email" placeholder="Email" />
+            <textarea rows="5" placeholder="Message" />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </div>
   )
 }
 
